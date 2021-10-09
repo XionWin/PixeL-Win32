d2eb1b1509c006a53369664dbc80e0e803e9c372
XionWin <xionwin@hotmail.com>
2021-10-09 18:25:29 +0800

    Update

--- Pixel/Program.cs
+++ Pixel/Program.cs
@@ -8,7 +8,7 @@ namespace Pixel
         {
             Console.WriteLine("Hello World!");
 
-            var window = new Win32.Win32Window("OpenGL ES 3.0", 800, 600);
+            var window = new Win32.NativeWindow("OpenGL ES 3.0", 800, 600);
             window.SetLocation(0, 0);
             window.Show();
         }
