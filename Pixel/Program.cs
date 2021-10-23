48e9d3929f0f5309c4ffa3b572adf7c3ca810898
Xion <xionwin@hotmail.com>
2021-10-23 20:21:01 +0800

    Update

--- Pixel/Program.cs
+++ Pixel/Program.cs
@@ -10,18 +10,7 @@ namespace Pixel
 
             var window = new Win32.Window("OpenGL ES 3.0", 800, 600);
             window.SetLocation(100, 100);
-            System.Threading.Tasks.Task.Run(() => {
-                var counter = 0;
-               while (true)
-               {
-                    window.Title = $"Hello world {counter}";
-                    counter++;
-                    System.Threading.Thread.Sleep(1000);
-               } 
-            });
             window.Show();
         }
     }
 }
-
-
