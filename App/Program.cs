eba9a431a4b187ab01a5e46c3f6268bd8298e18d
Xion <xionwin@hotmail.com>
2021-11-04 23:51:55 +0800

    Update

--- App/Program.cs
+++ App/Program.cs
@@ -30,43 +30,66 @@ namespace App
             var shader = new PixelGLShader();
 
             
-            uint size = 3;
+            uint size = 6;
             const float TRIANGLE_SIZE = 0.8f;
 
             var vertices = new Vertex[size];
-            vertices[0].x = -.5f;
-            vertices[0].y = .5f;
+            vertices[0].x = 0f;
+            vertices[0].y = 0f;
             vertices[0].r = 1f;
             vertices[0].g = .0f;
             vertices[0].b = .0f;
             vertices[0].a = 1f;
 
-            vertices[1].x = -.5f;
-            vertices[1].y = -.5f;
+            vertices[1].x = 0f;
+            vertices[1].y = -.25f;
             vertices[1].r = .0f;
             vertices[1].g = 1f;
             vertices[1].b = .0f;
             vertices[1].a = 1f;
 
-            vertices[2].x = .5f;
-            vertices[2].y = -.5f;
+            vertices[2].x = -.25f;
+            vertices[2].y = 0f;
             vertices[2].r = .0f;
             vertices[2].g = .0f;
             vertices[2].b = 1f;
             vertices[2].a = 1f;
+
+            
+            vertices[3].x = 0f;
+            vertices[3].y = 0f;
+            vertices[3].r = 1f;
+            vertices[3].g = .0f;
+            vertices[3].b = .0f;
+            vertices[3].a = 1f;
+
+            vertices[4].x = 0f;
+            vertices[4].y = .25f;
+            vertices[4].r = .0f;
+            vertices[4].g = 1f;
+            vertices[4].b = .0f;
+            vertices[4].a = 1f;
+
+            vertices[5].x = .25f;
+            vertices[5].y = 0f;
+            vertices[5].r = .0f;
+            vertices[5].g = .0f;
+            vertices[5].b = 1f;
+            vertices[5].a = 1f;
             
             var indices = new short[]
             {
-                0, 1, 2
+                0, 1, 2, 
+                3, 4, 5
             };
 
             var vbos = new uint[2];
 
-            for (int i = 0; i < size; i++)
-            {
-                vertices[i].x = (float)Math.Cos(-i * (2.0f * Math.PI / (size))) * TRIANGLE_SIZE;
-                vertices[i].y = (float)Math.Sin(-i * (2.0f * Math.PI / (size))) * TRIANGLE_SIZE;
-            }
+            // for (int i = 0; i < size; i++)
+            // {
+            //     vertices[i].x = (float)Math.Cos(-i * (2.0f * Math.PI / (size))) * TRIANGLE_SIZE;
+            //     vertices[i].y = (float)Math.Sin(-i * (2.0f * Math.PI / (size))) * TRIANGLE_SIZE;
+            // }
 
             OpenGLES.GL.GenBuffers((uint)vbos.Length, out vbos);
             OpenGLES.GL.glBindBuffer(OpenGLES.Def.BufferTarget.ArrayBuffer, vbos[0]);
@@ -103,7 +126,8 @@ namespace App
 
                         // SetRotationMatrix(angle / 360d * Math.PI * 2, model_mat_location);
                         // OpenGLES.GL.glDrawElements(OpenGLES.Def.BeginMode.Triangles, 3, OpenGLES.Def.DrawElementsType.UnsignedShort, 0);
-                        OpenGLES.GL.glDrawArrays(OpenGLES.Def.BeginMode.TriangleFan, 0, size);
+                        OpenGLES.GL.glDrawArrays(OpenGLES.Def.BeginMode.TriangleFan, 0, 3);
+                        OpenGLES.GL.glDrawArrays(OpenGLES.Def.BeginMode.TriangleFan, 3, 3);
                     }
                 }
 
